b4bfb854162a2eefd927e4047ceb31cc969cb790
Gabriela <123262079+gabiboot@users.noreply.github.com>
2024-02-24 12:00:10 -0300

    Add files via upload

---
+++ returning-values.py
@@ -0,0 +1,34 @@
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(sorted(time_list))
+
+
+time_list = [12, 2, 32, 19, 57, 22, 14]
+print(min(time_list))
+print(max(time_list))
+
+#----------------------------------------------------------#
+
+def area_triangle(base, height):
+    return base*height/2
+area_a = area_triangle(5,4)
+area_b = area_triangle(7,3)
+sum = area_a + area_b
+print("The sum of both areas is: " + str(sum))
+
+#------------------------------------------------------------#
+
+def convert_seconds(seconds):
+    hours = seconds // 3600
+    minutes = (seconds - hours * 3600) // 60
+    remaining_seconds = seconds - hours * 3600 - minutes * 60
+    return hours, minutes, remaining_seconds
+ 
+hours, minutes, seconds = convert_seconds(5000)
+print(hours, minutes, seconds)
+
+#------------------------------------------------------------#
+
+def greeting(name):
+    print("Welcome, " + name)
+result = greeting("Christine")
+print(result)
